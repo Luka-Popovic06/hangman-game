3aabc166c35853d589a388bfee61c5a303947e74
Luka Popovic <lukapopovic0111@gmail.com>
2025-12-01 22:35:41 +0100

    Add Button component

--- Button.jsx
+++ Button.jsx
@@ -1,1 +1,14 @@
+import { useState } from "react";
+const Button = ({ children, btnType, variation, clickAction }) => {
+  let btnClass;
+  if (variation === "primary") btnClass = "primary-btn";
+  if (variation === "primary-red") btnClass = "primary-btn active-red";
+  if (variation === "secondary") btnClass = "secondary-btn";
 
+  return (
+    <button type={btnType} className={btnClass} onClick={clickAction}>
+      {children}
+    </button>
+  );
+};
+export default Button;
