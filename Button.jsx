9718787979b71dc9a1a9314178a773f857b705c8
Luka Popovic <lukapopovic0111@gmail.com>
2025-12-04 19:48:28 +0100

    Update Button component

--- Button.jsx
+++ Button.jsx
@@ -1,1 +1,20 @@
+const Button = ({ children, btnType, variation, clickAction }) => {
+  let btnClass;
+  if (variation === "primary") btnClass = "primary-btn";
+  if (variation === "primary-red") btnClass = "primary-btn active-red";
+  if (variation === "secondary") btnClass = "secondary-btn";
+  if (variation === "") btnClass = "btn";
+  if (variation === "secondary-red") btnClass = "secondary-red-btn";
 
+  return (
+    <button
+      type={btnType}
+      disabled={btnClass === "secondary-red-btn" ? true : false}
+      className={btnClass}
+      onClick={clickAction}
+    >
+      {children}
+    </button>
+  );
+};
+export default Button;
